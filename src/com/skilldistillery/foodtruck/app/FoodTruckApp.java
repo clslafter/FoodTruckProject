febc57b10e01ca6633aeee8b8fb96874fd59c1e8
clslafter <clslafter@att.net>
2022-08-27 12:40:49 -0500

    FoodTruckApp with FoodTruck import, Array declared, Max Trucks and numOfMaxTrucks created, user input loop method and addTruck method

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,15 +1,52 @@
 package com.skilldistillery.foodtruck.app;
 
 import com.skilldistillery.foodtruck.entities.FoodTruck;
+import java.util.Scanner;
 
 public class FoodTruckApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner kb = new Scanner(System.in);
 
-		FoodTruck charlie = new FoodTruck("Charlie's", "Mexican", 5);
-		System.out.println(charlie.toString());
+	final int MAX_TRUCKS = 5;
+	int numOfTrucks = 0;
+	FoodTruck[] foodTrucks = new FoodTruck[MAX_TRUCKS];
+
+	public static void main(String[] args) {
+
+		FoodTruckApp app = new FoodTruckApp();
 		
+		app.inputFoodTrucks();
+
 	}
 
+	public void inputFoodTrucks() {
+
+		while (numOfTrucks < MAX_TRUCKS) {
+			System.out.println("Please enter the Food Truck information."
+					+ "\nTo quit, type quit when prompted for the name");
+			System.out.print("Food Truck Name: ");
+			String name = kb.next();
+			if(name.equalsIgnoreCase("quit")) {
+				break;
+			}
+			System.out.print("Food Truck Type: ");
+			String type = kb.next();
+			System.out.print("Food Truck Rating: ");
+			int rating = kb.nextInt();
+			
+			FoodTruck truck = new FoodTruck(name, type, rating);
+			
+			addTruck(truck);
+			
+		}
+		
+
+	}
+	
+	public void addTruck (FoodTruck truck) {
+		if (numOfTrucks < MAX_TRUCKS) {
+			foodTrucks[numOfTrucks] = truck;
+			numOfTrucks++;
+			}
+	}
 }
