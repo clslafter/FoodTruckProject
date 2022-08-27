21003fbfda5ff408f94183be31e76c0361cc71a9
clslafter <clslafter@att.net>
2022-08-27 11:32:38 -0500

    Food Truck Class fields and methods, including getters, setters, and toString plus contructor assigning id field

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,15 @@
 package com.skilldistillery.foodtruck.app;
 
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		FoodTruck charlie = new FoodTruck("Charlie's", "Mexican", 5);
+		System.out.println(charlie.toString());
+		
 	}
 
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -2,4 +2,56 @@ package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
 
+	private int id;
+	private String name;
+	private String type;
+	private int rating;
+	private static int numOfFoodTrucks = 0;
+
+	public FoodTruck(String ftName, String ftType, int ftRating) {
+		name = ftName;
+		type = ftType;
+		rating = ftRating;
+
+//		assign Food Truck Id Number and increment
+		id = ++numOfFoodTrucks;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder foodTruckInfo = new StringBuilder();
+		foodTruckInfo.append("Food Truck ID: ").append(id).append("\nFood Truck Name: ").append(name)
+				.append("\nFood Truck Type: ").append(type).append("\nFood Truck Rating: ").append(rating);
+
+		return foodTruckInfo.toString();
+	}
+
 }
